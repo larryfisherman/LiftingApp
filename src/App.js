8d2d10c680576684deb3c3c96621c14285cf656a
albertkarkutex7 <31852158+larryfisherman@users.noreply.github.com>
2023-01-27 14:21:13 +0100

    squash commit pls work

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Workouts from "./components/Workouts";
 import WorkoutDetails from "./components/WorkoutDetails";
 
 function App() {
+  console.log("nowy commit");
   return (
     <div className="App">
       <Router>
